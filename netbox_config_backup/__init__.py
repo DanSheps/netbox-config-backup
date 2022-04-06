2f8aa12eb647dd86cece4ec27c2dfc136bcf49fd
Daniel Sheppard <dans@dansheps.com>
2022-04-06 12:48:07 -0500

    Update for 3.2

--- netbox_config_backup/__init__.py
+++ netbox_config_backup/__init__.py
@@ -17,6 +17,8 @@ class NetboxConfigBackup(PluginConfig):
     author = metadata.get('Author')
     author_email = metadata.get('Author-email')
     base_url = 'configbackup'
+    min_version = '3.2.0'
+    max_version = '3.2.99'
     required_settings = [
         'repository',
         'committer',
