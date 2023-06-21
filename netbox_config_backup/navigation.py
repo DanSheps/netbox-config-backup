8345aa95be015113a8cb280a394c61345cb49ed2
Daniel Sheppard <dans@dansheps.com>
2023-06-21 08:41:49 -0500

    Fixes #70 - Add menu permissions

--- netbox_config_backup/navigation.py
+++ netbox_config_backup/navigation.py
@@ -2,21 +2,22 @@ from extras.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
 from utilities.choices import ButtonColorChoices
 
 item = PluginMenuItem(
-        link='plugins:netbox_config_backup:backup_list',
-        link_text='Devices',
-        buttons=[
-            PluginMenuButton(
-                link="plugins:netbox_config_backup:backup_add",
-                title="Add",
-                icon_class="mdi mdi-plus",
-                color=ButtonColorChoices.GREEN,
-            ),
-        ]
-    )
+    link='plugins:netbox_config_backup:backup_list',
+    link_text='Devices',
+    permissions=['netbox_config_backup.view_backups'],
+    buttons=[
+        PluginMenuButton(
+            link="plugins:netbox_config_backup:backup_add",
+            title="Add",
+            icon_class="mdi mdi-plus",
+            color=ButtonColorChoices.GREEN,
+        ),
+    ]
+)
 
 menu = PluginMenu(
-        label="Configuration Backup",
-        groups=(
-            ('Backup Jobs', (item,)),
-        )
+    label="Configuration Backup",
+    groups=(
+        ('Backup Jobs', (item,)),
     )
+)
