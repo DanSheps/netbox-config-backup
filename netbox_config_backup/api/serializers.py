e852ca5978b7490216f1ca13f431d2637072b107
Daniel Sheppard <dans@dansheps.com>
2023-06-21 08:38:53 -0500

    Fixes #69 - Fix errant API serializer hyperlinks

--- netbox_config_backup/api/serializers.py
+++ netbox_config_backup/api/serializers.py
@@ -13,7 +13,7 @@ __all__ = (
 
 
 class BackupSerializer(NetBoxModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_sync-api:sync-detail')
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_config_backup-api:sync-detail')
     device = NestedDeviceSerializer()
     ip = NestedIPAddressSerializer()
 
